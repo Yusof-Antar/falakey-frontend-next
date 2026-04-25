0c41fc6b93cea87eb45fe5742670ca825adb8720
yusof_antar <youssof.antar@gmail.com>
2026-04-25 15:36:02 +0300

    test vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
@@ -8,7 +7,8 @@ const nextConfig: NextConfig = {
     ],
   },
   typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
